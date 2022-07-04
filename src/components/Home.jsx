f62222c6aab18b766350ca002f0fe0e3d5bceb6a
Josh <joshmiranda.exe@gmail.com>
2022-07-03 20:17:01 -0400

    read file as array

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
   return (
     <div>
-        <h1>Home</h1>
+      <h1>Home</h1>
     </div>
-  )
-}
+  );
+};
--- src/components/ParseExcel.jsx
+++ src/components/ParseExcel.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
 
 export const ParseExcel = () => {
+  const [fileName, setFileName] = useState(null);
 
-  const handleFile = (e) => {
-    console.log(e.target.files[0]);
+  const handleFile = async (e) => {
+    // read file
+    const file = e.target.files[0];
 
-  }
+    // set file name
+    setFileName(file.name);
 
+    // take contents of file and insert it into array buffer
+    const data = await file.arrayBuffer();
 
+    // parse through the workbook
+    const workbook = XLSX.read(data);
 
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+    const jsonData = XLSX.utils.sheet_to_json(worksheet)
 
+    console.log(jsonData);
+  };
 
   return (
     <div>
-        <h1>Parse</h1>
-        <input type="file" onChange={(e) => handleFile(e)}/>
+      <h1>Parse</h1>
+
+      {fileName && (
+        <p>
+          FileName: <span>{fileName}</span>
+        </p>
+      )}
+
+      <input type="file" onChange={(e) => handleFile(e)} />
     </div>
-  )
-}
+  );
+};
