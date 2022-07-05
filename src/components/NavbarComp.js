03049894b0d9a011cae71a201a82946d0a82e8ac
Josh <joshmiranda.exe@gmail.com>
2022-07-05 18:24:35 -0400

    parse page

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NavbarComp = () => (
   <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
--- src/components/ParseExcel.js
+++ src/components/ParseExcel.js
@@ -1,5 +1,8 @@
+import "../styles/ParseExcel.css";
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 export const ParseExcel = () => {
   const [fileName, setFileName] = useState(null);
@@ -17,23 +20,25 @@ export const ParseExcel = () => {
     // parse through the workbook
     const workbook = XLSX.read(data);
 
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-    const jsonData = XLSX.utils.sheet_to_json(worksheet)
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
     console.log(jsonData);
   };
 
   return (
-    <div>
-      <h1>Parse</h1>
-
-      {fileName && (
-        <p>
-          FileName: <span>{fileName}</span>
-        </p>
-      )}
-
-      <input type="file" onChange={(e) => handleFile(e)} />
-    </div>
+    <>
+      <div className="body">
+        <div className="drag-area">
+          <div className="icon">
+            <FontAwesomeIcon icon={faCloudArrowUp} />
+          </div>
+          <header>Upload File</header>
+          <label>
+            <input type="file" onChange={(e) => handleFile(e)} />
+          </label>      
+        </div>
+      </div>
+    </>
   );
 };
