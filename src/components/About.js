597d6f846caecd923d37675e553405a9884443e5
Josh <joshmiranda.exe@gmail.com>
2022-07-07 00:30:03 -0400

    About and Contact WIP

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,109 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
   render() {
     return (
-      <div>About</div>
-    )
+      <main className="page-wrapper">
+        <section class="container pb-5 mb-md-2 mb-lg-4">
+          <h1 class="border-bottom pb-4">Who We Are</h1>
+          <div class="row pt-2 pt-md-3">
+            <div class="col-md-6">
+              <p class="fs-lg pe-lg-4 mb-1 mb-lg-4">
+                Mi semper risus ultricies orci pulvinar in at enim orci. Quis
+                facilisis nunc pellentesque in ullamcorper sit. Lorem blandit
+                arcu sapien, senectus libero, amet dapibus cursus quam. Eget
+                pellentesque eu purus volutpat adipiscing malesuada. Purus nisi,
+                tortor vel lacus. Donec diam molestie ultrices vitae eget
+                pulvinar fames. Velit lacus mi purus velit justo, amet. Nascetur
+                lobortis diam, duis orci. Vitae nibh amet lorem pellentesque.
+              </p>
+            </div>
+            <div class="col-md-6">
+              <p class="fs-lg ps-lg-4 mb-1 mb-lg-4">
+                Ac at sed sit senectus massa. Massa ante amet ultrices magna
+                porta tempor. Aliquet diam in et magna ultricies mi at. Lectus
+                enim, vel enim egestas nam pellentesque et leo. Elit mi faucibus
+                laoreet aliquam pellentesque sed aliquet integer massa. Orci leo
+                tortor ornare id mattis auctor aliquam volutpat aliquet. Odio
+                lectus viverra eu blandit nunc malesuada vitae eleifend
+                pulvinar. In ac fermentum sit in orci.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="container pb-5 mb-2 mb-md-4 mb-lg-5">
+        <div class="bg-secondary rounded-3 py-5 px-3 px-md-0">
+          <div class="row justify-content-center mb-2 py-md-2 py-lg-4">
+            <div class="col-lg-10 col-md-11">
+              <h2 class="pb-3">Overpass by Numbers</h2>
+              <div class="row row-cols-1 row-cols-md-2 g-4">
+
+                
+                <div class="col">
+                  <div class="card h-100 border-0 shadow-sm p-md-2 p-lg-4">
+                    <div class="card-body d-flex align-items-center">
+                      <div class="bg-primary shadow-primary rounded-3 flex-shrink-0 p-3">
+                        <img src="/images/job.svg" class="d-block m-1" width="40" alt="Icon"/>
+                      </div>
+                      <div class="ps-4 ms-lg-3">
+                        <h3 class="display-5 mb-1">2,480</h3>
+                        <p class="mb-0"><span class="fw-semibold">Remote</span> Professionals</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                
+                <div class="col">
+                  <div class="card h-100 border-0 shadow-sm p-md-2 p-lg-4">
+                    <div class="card-body d-flex align-items-center">
+                      <div class="bg-primary shadow-primary rounded-3 flex-shrink-0 p-3">
+                        <img src="/images/job.svg" class="d-block m-1" width="40" alt="Icon"/>
+                      </div>
+                      <div class="ps-4 ms-lg-3">
+                        <h3 class="display-5 mb-1">1,200</h3>
+                        <p class="mb-0"><span class="fw-semibold">Requests</span> per Week</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                
+                <div class="col">
+                  <div class="card h-100 border-0 shadow-sm p-md-2 p-lg-4">
+                    <div class="card-body d-flex align-items-center">
+                      <div class="bg-primary shadow-primary rounded-3 flex-shrink-0 p-3">
+                        <img src="/images/job.svg" class="d-block m-1" width="40" alt="Icon"/>
+                      </div>
+                      <div class="ps-4 ms-lg-3">
+                        <h3 class="display-5 mb-1">760</h3>
+                        <p class="mb-0"><span class="fw-semibold">New Clients</span> per Month</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                
+                <div class="col">
+                  <div class="card h-100 border-0 shadow-sm p-md-2 p-lg-4">
+                    <div class="card-body d-flex align-items-center">
+                      <div class="bg-primary shadow-primary rounded-3 flex-shrink-0 p-3">
+                        <img src="/images/job.svg" class="d-block m-1" width="40" alt="Icon"/>
+                      </div>
+                      <div class="ps-4 ms-lg-3">
+                        <h3 class="display-5 mb-1">58</h3>
+                        <p class="mb-0"><span class="fw-semibold">Countries</span> Using Our Product</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      </main>
+    );
   }
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,47 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <section class="container pb-5 mb-2 mb-md-4 mb-lg-5">
+        <div class="position-relative bg-secondary rounded-3 py-5 mb-2">
+          <div class="row pb-2 py-md-3 py-lg-5 px-4 px-lg-0 position-relative zindex-3">
+            <div class="col-xl-3 col-lg-4 col-md-5 offset-lg-1">
+              <p class="lead mb-2 mb-md-3">Ready to get started?</p>
+              <h2 class="h1 pb-3">Dont Hesitate to Contact Us</h2>
+            </div>
+            <form class="col-lg-6 col-md-7 offset-xl-1 needs-validation" >
+              <div class="row">
+                <div class="col-sm-6 mb-4">
+                  <label for="name" class="form-label fs-base">Full name</label>
+                  <input type="text" id="name" class="form-control form-control-lg" required=""/>
+                  <div class="invalid-feedback">Please enter your name!</div>
+                </div>
+
+
+
+                <div class="col-sm-6 mb-4">
+                  <label for="email" class="form-label fs-base">Email</label>
+                  <input type="email" id="email" class="form-control form-control-lg" required=""/>
+                  <div class="invalid-feedback">Please provide a valid email address!</div>
+                </div>
+                <div class="col-12 pb-2 mb-4">
+                  <label for="message" class="form-label fs-base">Message</label>
+                  <textarea id="message" class="form-control form-control-lg" rows="4" required=""></textarea>
+                  <div class="invalid-feedback">Please enter your message!</div>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary shadow-primary btn-lg">Send request</button>
+            </form>
+          </div>
+
+          
+          
+        </div>
+      </section>
+
+
+
+    </div>
   )
 }
 
