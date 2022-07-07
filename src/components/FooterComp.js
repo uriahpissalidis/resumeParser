ae4fce3ca49b20c77037427b838dd2235f187782
Josh <joshmiranda.exe@gmail.com>
2022-07-06 23:06:27 -0400

    incomplete footer

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,64 +1,141 @@
 import React from "react";
 import "../styles/Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
-  <footer className="bg-dark text-center text-white page-footer font-small blue pt-4">
-    <div className="container-fluid text-center text-md-left">
-      <div className="row">
-        <div className="col-md-6 mt-md-0 mt-3">
-          <h5 className="text-uppercase">ResConnect</h5>
-          <p>
+  <footer class="py-8 py-md-11 bg-dark text-center text-white page-footer font-small blue pt-4" >
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3">
+          <p class="text-gray-700 mb-2">
             Here with our resume parser you can find jobs and connect with
             recruiters. Increase your chances of getting that job and gain
             confidence now.
           </p>
-        </div>
 
-        <hr className="clearfix w-100 d-md-none pb-0" />
-
-        <div className="col-md-3 mb-md-0 mb-3">
-          <h5 className="text-uppercase">Website</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">Apps</a>
+          <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
+            <li class="list-inline-item list-social-item me-3">
+              <Link as={Link} class="text-decoration-none" to="/parse">
+                Check Resume
+              </Link>
             </li>
-            <li>
-              <a href="#!">Site Stats</a>
+            <li class="list-inline-item list-social-item me-3">
+              <Link as={Link} class="text-decoration-none" to="/parse">
+                Check Resume
+              </Link>
             </li>
-            <li>
-              <a href="#!">Recommendations</a>
+            <li class="list-inline-item list-social-item me-3">
+              <Link as={Link} class="text-decoration-none" to="/parse">
+                Check Resume
+              </Link>
             </li>
-            <li>
-              <a href="#!">API</a>
+            <li class="list-inline-item list-social-item">
+              <Link as={Link} class="text-decoration-none" to="/parse">
+                Check Resume
+              </Link>
             </li>
           </ul>
         </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <h6 class="fw-bold text-uppercase text-gray-700">Products</h6>
 
-        <div className="col-md-3 mb-md-0 mb-3">
-          <h5 className="text-uppercase">Company</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">About</a>
+          <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Page Builder
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                UI Kit
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Styleguide
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Documentation
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <h6 class="fw-bold text-uppercase text-gray-700">Services</h6>
+
+          <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Documentation
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Changelog
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Pagebuilder
+              </a>
             </li>
             <li>
-              <a href="#!">Contact</a>
+              <a href="#!" class="text-reset">
+                UI Kit
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">
+          <h6 class="fw-bold text-uppercase text-gray-700">Connect</h6>
+
+          <ul class="list-unstyled text-muted mb-0">
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Page Builder
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                UI Kit
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Styleguide
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Documentation
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <h6 class="fw-bold text-uppercase text-gray-700">Legal</h6>
+
+          <ul class="list-unstyled text-muted mb-0">
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Documentation
+              </a>
+            </li>
+            <li class="mb-3">
+              <a href="#!" class="text-reset">
+                Changelog
+              </a>
             </li>
             <li>
-              <a href="#!">Jobs</a>
-            </li>
-            <li>
-              <a href="#!">Sponsors</a>
+              <a href="#!" class="text-reset">
+                Pagebuilder
+              </a>
             </li>
           </ul>
         </div>
       </div>
-    </div>
-
-    
-
-    <div className="footer-copyright text-center py-3">
-      © 2022 Copyright:
-      <p>Josh Miranda</p>
     </div>
   </footer>
 );
