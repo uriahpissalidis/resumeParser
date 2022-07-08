cb255a3220127c0e9cd9011156d99f09fe61e4cf
Josh <joshmiranda.exe@gmail.com>
2022-07-08 01:23:25 -0400

    about

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,38 @@
 import React from 'react'
+import "../styles/Contact.css";
 
 function Contact() {
   return (
-    <div>
-      <section class="container pb-5 mb-2 mb-md-4 mb-lg-5">
-        <div class="position-relative bg-secondary rounded-3 py-5 mb-2">
-          <div class="row pb-2 py-md-3 py-lg-5 px-4 px-lg-0 position-relative zindex-3">
-            <div class="col-xl-3 col-lg-4 col-md-5 offset-lg-1">
-              <p class="lead mb-2 mb-md-3">Ready to get started?</p>
-              <h2 class="h1 pb-3">Dont Hesitate to Contact Us</h2>
+    <main className="page-wrapper">
+      <section className="container pb-5 mb-2 mb-md-4 mb-lg-5">
+        <div className="card rounded-3 py-5 mb-2">
+          <div className="row pb-2 py-md-3 py-lg-5 px-4 px-lg-0 position-relative zindex-3">
+            <div className="col-xl-3 col-lg-4 col-md-5 offset-lg-1">
+              <p className="lead mb-2 mb-md-3">Ready to get started?</p>
+              <h2 className="h1 pb-3">Don't Hesitate to Contact Us</h2>
             </div>
-            <form class="col-lg-6 col-md-7 offset-xl-1 needs-validation" >
-              <div class="row">
-                <div class="col-sm-6 mb-4">
-                  <label for="name" class="form-label fs-base">Full name</label>
+            <form className="col-lg-6 col-md-7 offset-xl-1 needs-validation" >
+              <div className="row">
+                <div className="col-sm-6 mb-4">
+                  <label for="name" className="form-label fs-base">Full name</label>
                   <input type="text" id="name" class="form-control form-control-lg" required=""/>
-                  <div class="invalid-feedback">Please enter your name!</div>
+                  <div className="invalid-feedback">Please enter your name!</div>
                 </div>
 
 
 
-                <div class="col-sm-6 mb-4">
-                  <label for="email" class="form-label fs-base">Email</label>
-                  <input type="email" id="email" class="form-control form-control-lg" required=""/>
-                  <div class="invalid-feedback">Please provide a valid email address!</div>
+                <div className="col-sm-6 mb-4">
+                  <label for="email" className="form-label fs-base">Email</label>
+                  <input type="email" id="email" className="form-control form-control-lg" required=""/>
+                  <div className="invalid-feedback">Please provide a valid email address!</div>
                 </div>
-                <div class="col-12 pb-2 mb-4">
-                  <label for="message" class="form-label fs-base">Message</label>
-                  <textarea id="message" class="form-control form-control-lg" rows="4" required=""></textarea>
-                  <div class="invalid-feedback">Please enter your message!</div>
+                <div className="col-12 pb-2 mb-4">
+                  <label for="message" className="form-label fs-base">Message</label>
+                  <textarea id="message" className="form-control form-control-lg" rows="4" required=""></textarea>
+                  <div className="invalid-feedback">Please enter your message!</div>
                 </div>
               </div>
-              <button type="submit" class="btn btn-primary shadow-primary btn-lg">Send request</button>
+              <button type="submit" className="btn btn-primary shadow-primary btn-lg">Send request</button>
             </form>
           </div>
 
@@ -42,7 +43,7 @@ function Contact() {
 
 
 
-    </div>
+    </main>
   )
 }
 
